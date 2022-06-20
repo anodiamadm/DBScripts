63d138be2a1ab896d481d4b35fbf9461798b4372
unknown <anodiam.adm@gmail.com>
2022-06-20 14:46:59 +1000

    Update InitialScript20220410.sql

--- InitialScript20220410.sql
+++ InitialScript20220410.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS anodiam_student;
-CREATE DATABASE anodiam_student;
-USE anodiam_student;
+DROP DATABASE IF EXISTS anodiamstudent;
+CREATE DATABASE anodiamstudent;
+USE anodiamstudent;
 CREATE TABLE `mst_board` (
   `board_id` bigint NOT NULL AUTO_INCREMENT,
   `board_code` varchar(255) NOT NULL,
